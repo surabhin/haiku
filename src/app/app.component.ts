2e55e553a8dc6edf5b0d937fef7660581a40c999
nikithaa <nikithaa@PRINHYLTPAP1434.local>
2021-04-29 14:24:48 +0530

    basic UI changes for todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'todo-list';
+  todoList = [
+    {title: 'Sprint Tasks', completed: false},
+    {title: 'Call Marketing Team', completed: false},
+    {title: 'Complete Survey', completed: false},
+    {title: 'Submit Timesheets', completed: false},
+    {title: 'Grocery Shopping', completed: false},
+  ];
+  reminderList = [
+    {title: 'Drink Water', completed: false},
+    {title: 'Blink your eye', completed: false},
+    {title: 'Go for a quick walk', completed: false}
+  ];
+  toggleTodo = false;
+  toggleReminder = false;
+  toggleBubble = false;
+
+  openBubble() {
+    this.toggleBubble = !this.toggleBubble;
+  }
+
+  openTodo() {
+    this.toggleTodo = !this.toggleTodo;
+  }
+
+  openReminder() {
+    this.toggleReminder = !this.toggleReminder;
+  }
+
+  completeItem(item) {
+    item.completed = !item.completed;
+  }
 }
